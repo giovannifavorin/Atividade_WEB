838fc7c0c3e9a9900e09d79ad522075a684b8bbe
Giovanni Favorin de Melo <80218840+giovannifavorin@users.noreply.github.com>
2023-08-16 10:27:36 -0300

    Add files via upload

---
+++ atividade1/verificaNumeroPrimo.js
@@ -0,0 +1,28 @@
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  if (number <= 3) {
+    return true;
+  }
+  if (number % 2 === 0 || number % 3 === 0) {
+    return false;
+  }
+  for (let i = 5; i * i <= number; i += 6) {
+    if (number % i === 0 || number % (i + 2) === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function printPrimeStatus(number) {
+  if (isPrime(number)) {
+    console.log("1 - O número é primo.");
+  } else {
+    console.log("0 - O número não é primo.");
+  }
+}
+
+const inputNumber = 33;
+printPrimeStatus(inputNumber);
